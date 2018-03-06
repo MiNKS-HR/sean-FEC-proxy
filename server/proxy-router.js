c4c3dadbb734ed39c0d41e05b8c4127442da3c42
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-05 20:55:52 -0800

    completer MVP proxy server

---
+++ server/proxy-router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = require('express-router'); 
+const axios = require('axios');
+const _ = require('lodash');
+
+
+const proxyRedirect = (ip, url, port) => {
+
+  return (req, res) => {
+    let axiosRequest;
+
+    console.log('req.type', req.method);
+
+    if (req.method === 'GET') {
+      axiosRequest = axios.get;
+    } else if (req.method === 'POST') {
+      axiosRequest = axios.post;
+    } else {
+      console.log('simple proxy can only hanle get and post requests');
+      return
+    }
+
+    console.log('sending request to ', ip + ':' + port + req.url);
+    console.log('req.params', req.body);
+
+    axiosRequest('http://' + ip + ':' + port + req.url, req.body)
+    .then((response) => {
+      console.log(response);
+      res.end(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(501);
+      res.end('proxy redirect Error');
+    });
+  }
+
+}
+
+
+const proxyRouter = (redirects) => {
+  let router = express.Router();
+
+  for (var key in redirects) {
+    router.get(redirects[key].url, proxyRedirect(redirects[key].redirectIP, redirects[key].url, redirects[key].redirectPort));
+    router.post(redirects[key].url, proxyRedirect(redirects[key].redirectIP, redirects[key].url, redirects[key].redirectPort));
+  }
+
+  return router;
+}
+
+module.exports.Redirect = proxyRedirect;
+module.exports.Router = proxyRouter;
+
+module.exports.doStuff = (req, res) => {
+  console.log('test request')
+  axios.get('127.0.0.1:3001/1/reviews', {}).then((res) => {console.log(res);});
+}
